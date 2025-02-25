dc09b14698863e457a09560ad7be2c21a519386f
RSP-92 <rsprincivil@gmail.com>
2025-02-25 15:13:54 -0500

    2 files, fixed readme to .md, added to-do-list to web_scrapping

---
+++ web_scrapping.py
@@ -0,0 +1,4 @@
+# Plan: 1) Use the BLS Public Data API to get JSON data of labor data
+# 2) Clean and organize the data into a sql table 
+# 3) Make a pipeline for the cleaned data
+# #Later, use beautiful soup to scrap CDC data, see if there's cross over between two data sets  
