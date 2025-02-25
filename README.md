59d4b87197ca1d28d91541de2627def6ac771fb6
RSP-92 <rsprincivil@gmail.com>
2025-02-25 17:29:39 -0500

    2 files, added api call

---
+++ README.md
@@ -0,0 +1,4 @@
+Notes to be cleaned later:
+
+* First created the API call
+*Then explored the data with a for loop to take a better look through the dictionary 
--- web_scrapping.py
+++ web_scrapping.py
@@ -2,3 +2,23 @@
 # 2) Clean and organize the data into a sql table 
 # 3) Make a pipeline for the cleaned data
 # #Later, use beautiful soup to scrap CDC data, see if there's cross over between two data sets  
+
+import requests 
+
+#Making API Call
+
+url = 'https://api.bls.gov/publicAPI/v1/timeseries/data/'
+headers = {'Content-type': 'application/json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+response_dict = r.json()
+
+#Print and analyze results
+print(response_dict.keys())
+
+
+#It was a success!
+
+for i in response_dict.keys():
+    print(response_dict.values)
+
