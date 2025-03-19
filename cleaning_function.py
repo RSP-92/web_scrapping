7dd9493f00a43ab1af03cfe69a627497aeb6632d
RSP-92 <rsprincivil@gmail.com>
2025-03-19 17:20:52 -0400

    made cleaning function

--- cleaning_function.py
+++ cleaning_function.py
@@ -3,36 +3,39 @@ import json
 import pandas as pd
 from pathlib import Path
 
-p = Path(r'data\readable_employee_data.json')
+def clean_data(pathway):
+    p = Path(pathway)
+    with p.open('r', encoding='utf-8') as f:
+        data = json.loads(f.read())
+    results_series = data['Results']['series']
+    results_data = results_series[0]
+    results_year = results_data['data']
+    all_data = []
 
-with p.open('r', encoding='utf-8') as f:
-    data = json.loads(f.read())
+    for year in results_year:
+        all_data.append(year)
+    df = pd.DataFrame(all_data)
+    print(df)
+    return df
 
-
-results_series = data['Results']['series']
 
 # print(type(results_series)) < returns a list
+clean_data(r'data\readable_employee_data.json')
 
-results_data = results_series[0]
 
 # print(type(results_data)) < returns a dict
 
 # print(f"\n{results_data['data']}")
 
-results_year = results_data['data']
+
 
 # print(f"\nThis is results year: {results_year}")
 
 # print(type(results_year)) < returns a list, I'm noticing a pattern
-all_data = []
 
-for year in results_year:
-    all_data.append(year)
 
-print(type(all_data))
 
-df = pd.DataFrame(all_data)
-print(df)
+    
 
 
 
